2c73debdf9a0033afc082a384734c55fd1264e54
DirkGentle <fabian.torres.alvarez@gmail.com>
2018-03-11 16:06:22 -0300

    Script no longer loops and runs forever

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ def load_log(mods_backup_path): #para recuperar los mods respaldados
 		return pickle.load(my_backup)
 
 def output_log(text): #lo uso para ver el output del bot
-	output_log_path = "/home/pi/Downloads/elGuardianBot/output_log.txt"
+	output_log_path = "output_log.txt"
 	with open(output_log_path, 'a') as myLog:
 		s = "[" +  datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "] "
 		s = s + text +  "\n"
@@ -44,43 +44,43 @@ def send_alert(mods, mods_old, reddit, current_subreddit, concerned_redditors):
 		reddit.redditor(concerned_redditor).message(subject, body)
 		output_log('To: ' + str(concerned_redditor))
 
+def main():
+	mods_backup_path = 'mods.pickle'
+	current_subreddit = 'Uruguay'
+	concerned_redditors = ['DirkGentle']
+
+	reddit = praw.Reddit(	client_id = login.client_id,
+							client_secret = login.client_secret,
+							password = login.password,
+							username = login.username,
+							user_agent = 'testcript for /u/elGuardianBot')
+	output_log('Login to reddit as: ' + reddit.user.me().name)
+
+	try:
+		mods_old = load_log(mods_backup_path)
+#		output_log('Successful load from backup file')
+	except:
+		mods_old = reddit.subreddit(current_subreddit).moderator()
+		update_log(mods_old, mods_backup_path)
+		output_log('Error reading file, getting mods from the internet')
+		output_log("Mods of /r/" + current_subreddit)
+		output_log(get_mods_format(mods_old))
+		return
+
+	mods = reddit.subreddit(current_subreddit).moderator()
+	if set(mods) != set(mods_old):
+		send_alert(mods, mods_old, reddit, current_subreddit, concerned_redditors)
+		update_log(mods, mods_backup_path)
+	else:
+		for i in range(len(mods)):
+			if mods[i].mod_permissions != mods_old[i].mod_permissions:
+				send_alert(mods, mods_old, reddit, current_subreddit, concerned_redditors)
+				update_log(mods, mods_backup_path)
+
+
 if __name__ == "__main__":
-	while True:
-		try:
-			mods_backup_path = '/home/pi/Downloads/elGuardianBot/mods.pickle'
-			current_subreddit = 'uruguay'
-			concerned_redditors = ['DirkGentle']
-
-			reddit = praw.Reddit(	client_id = login.client_id,
-								client_secret = login.client_secret,
-								password = login.password,
-								username = login.username,
-								user_agent = 'testcript for /u/elGuardianBot')
-			output_log('Login to reddit as: ' + reddit.user.me().name)
-
-			try:
-				mods_old = load_log(mods_backup_path)
-				output_log('Successful lod from backup file')
-			except:
-				mods_old = reddit.subreddit(current_subreddit).moderator()
-				update_log(mods_old, mods_backup_path)
-				output_log('Error reading file, getting mods from the internet')
-			output_log("Mods of /r/" + current_subreddit)
-			output_log(get_mods_format(mods_old))
-
-			while True:
-				mods = reddit.subreddit(current_subreddit).moderator()
-				if set(mods) != set(mods_old):
-					send_alert(mods, mods_old, reddit, current_subreddit, concerned_redditors)
-					update_log(mods, mods_backup_path)
-				else:
-					for i in range(len(mods)):
-						if mods[i].mod_permissions != mods_old[i].mod_permissions:
-							send_alert(mods, mods_old, reddit, current_subreddit, concerned_redditors)
-							update_log(mods_old, mods_backup_path)
-				mods_old = mods
-				time.sleep(1 * 60)
-
-		except Exception as exception:
-			output_log(str(exception))
-			output_log(traceback.format_exc())
+	try:
+		main()
+	except Exception as exception:
+		output_log(str(exception))
+		output_log(traceback.format_exc())
